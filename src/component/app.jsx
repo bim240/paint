e1ff1d02eab3f9af9d50a05dc16daacffc55bdde
bim240 <bimlendu357@gmail.com>
2020-03-11 19:06:56 +0530

    initial commit

---
+++ src/component/app.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import SelectShape from "./selectShape";
+
+const App = () => {
+  return (
+    <>
+      <SelectShape />
+    </>
+  );
+};
+
+export default App;
